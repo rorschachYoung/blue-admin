45ae131b0608d6c7317e6670f8cf0ad062f58a5b
rorschachYoung <878815924@qq.com>
2019-10-20 21:34:37 +0800

    添加gizp压缩打包优化

---
+++ dist/js/chunk-b8dd84c6.a0482460.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-b8dd84c6"],{"8cdb":function(n,t,s){"use strict";s.r(t);var c=function(){var n=this,t=n.$createElement,s=n._self._c||t;return s("h1",{staticClass:"notfound"},[n._v("\n  当前页不存在!\n  "),s("router-link",{staticClass:"link",attrs:{to:"/"}},[n._v("点击返回首页")])],1)},u=[],a={},e=a,i=(s("b2b1"),s("2877")),l=Object(i["a"])(e,c,u,!1,null,null,null);t["default"]=l.exports},b2b1:function(n,t,s){"use strict";var c=s("d3f3"),u=s.n(c);u.a},d3f3:function(n,t,s){}}]);
+//# sourceMappingURL=chunk-b8dd84c6.a0482460.js.map
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@
  * @type { import('@vue/cli-service').ProjectOptions}
   */
 
+const CompressionPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = ['js', 'css']
 module.exports = {
     // configureWebpack:{
     //     resolve:{
@@ -14,13 +16,26 @@ module.exports = {
     // chainWebpack(config){
     //     config.reslove.alias.set('$','@/assets')
     // },
-    publicPath:'/blue-admin',
-    devServer:{
-        proxy:{
-            '/api':{
-                target:'http://www.rorscloud.top:4000',
-                pathRewrite:{
-                    '^/api':''
+    configureWebpack: {
+        plugins: [
+            new CompressionPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'), // 只处理大于xx字节 的文件，默认：0
+                threshold: 10240,  // 示例：一个1024b大小的文件，压缩后大小为768b，minRatio : 0.75
+                minRatio: 0.8,
+                 // 是否删除源文件，默认: false
+                 deleteOriginalAssets: false
+              })
+        ]
+    },
+    publicPath: '/blue-admin',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://www.rorscloud.top:4000',
+                pathRewrite: {
+                    '^/api': ''
                 }
             }
         }
